42ca0f9e8795e095b5c4841696f80d2ab508b9a7
QiChunDong <1129300430@qq.com>
2024-08-08 23:04:36 +0800

    add:将data的数据代理到vm上，依然还是使用了defineProperty实现

--- src/initState.js
+++ src/initState.js
@@ -29,8 +29,22 @@ function initData(vm) {
   /* 此处如果直接执行data，this的指向就不再是vm了，vue实例中就无法通过this获取data中的变量了 */
   // data = vm._data = typeof data === 'function' ? data() : data;
   data = vm._data = typeof data === "function" ? data.call(vm) : data;
+  for (const key in data) {
+    proxy(vm, '_data', key);
+  }
   // 开始劫持data数据
   observer(data);
+}
+
+function proxy(vm, source, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[source][key]
+    },
+    set(newValue) {
+      vm[source][key] = newValue
+    }
+  })
 }
 
 function initWatch(vm) {}
